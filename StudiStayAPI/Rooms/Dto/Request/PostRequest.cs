b1d41bd962b11261f1bbe620a4da3fb492a3fbc1
Jamutaq <jamutaqortegavelez@gmail.com>
2023-10-18 23:49:03 -0500

    add: data annotations

--- StudiStayAPI/Rooms/Dto/Request/PostRequest.cs
+++ StudiStayAPI/Rooms/Dto/Request/PostRequest.cs
@@ -1,15 +1,33 @@
-﻿namespace StudiStayAPI.Rooms.Dto.Request;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudiStayAPI.Rooms.Dto.Request;
 
 /// <summary>
 /// Esta clase se usa para mapear el request body a un objeto C# (la entidad Post)
 /// </summary>
 public class PostRequest
 {
+    [Required]
+    [MaxLength(80)]
     public string Title { get; set; }
+    
+    [Required]
+    [MinLength(20)]
+    [MaxLength(200)]
     public string Description { get; set; }
+    
+    [Required]
     public string Address { get; set; }
+
+    [Required]
+    [Range(3, 1000)]
     public float Price { get; set; }
+    
     public float Rating { get; set; }
+    
+    [Required]
     public string NearestUniversity { get; set; }
+    
+    [Required]
     public int UserId { get; set; }
 }
--- StudiStayAPI/Rooms/Dto/Request/UserRequest.cs
+++ StudiStayAPI/Rooms/Dto/Request/UserRequest.cs
@@ -1,12 +1,24 @@
-﻿namespace StudiStayAPI.Rooms.Dto.Request;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudiStayAPI.Rooms.Dto.Request;
 
 /// <summary>
 /// Esta clase se usa para mapear el request body a un objeto C# (la entidad User)
 /// </summary>
 public class UserRequest
 {
+    [Required]
+    [MaxLength(80)]
     public string FullName { get; set; }
+    
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+    
+    [Required]
+    [MinLength(5)]
+    [MaxLength(20)]
     public string Password { get; set; }
+    
     public string Phone { get; set; }
 }
