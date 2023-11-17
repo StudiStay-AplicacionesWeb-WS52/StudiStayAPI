d498d13ad0ae22e71e774d5353191168225173d0
Paolo Sebastián Checa Apolinario <u202112749@upc.edu.pe>
2023-11-17 17:57:23 -0500

    feature: add exception handler
    Co-Authored-By: Angel-2002 <112642775+Angel-2002@users.noreply.github.com>
    Co-Authored-By: Ray Alessandro <ray_punkeki@hotmail.com>
    Co-Authored-By: yancefranco <105954764+yancefranco@users.noreply.github.com>

---
+++ StudiStayAPI/Shared/Exceptions/AppException.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Net;
+
+namespace StudiStayAPI.Shared.Exceptions;
+
+/// <summary>
+/// Excepción personalizada para la aplicación
+/// </summary>
+public class AppException : Exception
+{
+    public HttpStatusCode StatusCode { get; set; }
+
+    public AppException(HttpStatusCode statusCode, string message = "") : base(message)
+    {
+        StatusCode = statusCode;
+    }
+
+    public AppException(HttpStatusCode statusCode, string message, params object[] args) 
+        : base(string.Format(CultureInfo.CurrentCulture, message, args))
+    {
+        StatusCode = statusCode;
+    }
+}
