ffb81e362ccac4c946d2b51abfb49fd974ef3d4f
Paolo Sebastián Checa Apolinario <u202112749@upc.edu.pe>
2023-11-17 17:57:42 -0500

    feature: add error handler middlerware
    Co-Authored-By: Angel-2002 <112642775+Angel-2002@users.noreply.github.com>
    Co-Authored-By: Ray Alessandro <ray_punkeki@hotmail.com>
    Co-Authored-By: yancefranco <105954764+yancefranco@users.noreply.github.com>

---
+++ StudiStayAPI/Shared/Exceptions/ErrorHandlerMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using StudiStayAPI.Shared.Exceptions;
+
+namespace StudiStayAPI.Shared.Exceptions;
+
+/// <summary>
+/// Manejador de errores personalizado para la aplicación
+/// </summary>
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            var statusCode = StatusCodes.Status500InternalServerError; // Código por defecto
+            var message = error.Message;
+
+            if (error is AppException appException)
+            {
+                statusCode = (int)appException.StatusCode; // Usa el código de estado de AppException
+                message = appException.Message;
+            }
+
+            response.StatusCode = statusCode;
+            var result = JsonSerializer.Serialize(new { message = error?.Message });
+            await response.WriteAsync(result);
+        }
+    }
+}
