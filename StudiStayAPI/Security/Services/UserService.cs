0c37cee464d6cc7cdbb228f1d08524331c9d4669
Paolo Sebastián Checa Apolinario <u202112749@upc.edu.pe>
2023-11-17 17:55:12 -0500

    feature: user service
    Co-Authored-By: Angel-2002 <112642775+Angel-2002@users.noreply.github.com>
    Co-Authored-By: Ray Alessandro <ray_punkeki@hotmail.com>
    Co-Authored-By: yancefranco <105954764+yancefranco@users.noreply.github.com>

---
+++ StudiStayAPI/Security/Services/UserService.cs
@@ -0,0 +1,137 @@
+﻿using System.Net;
+using AutoMapper;
+using StudiStayAPI.Rooms.Domain.Repositories;
+using StudiStayAPI.Security.Domain.Models;
+using StudiStayAPI.Security.Domain.Repositories;
+using StudiStayAPI.Security.Domain.Services;
+using StudiStayAPI.Security.Domain.Services.Communication;
+using StudiStayAPI.Security.Jwt;
+using StudiStayAPI.Shared.Exceptions;
+using BCryptNet = BCrypt.Net.BCrypt;
+
+namespace StudiStayAPI.Security.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository userRepository;
+    private readonly IUnitOfWork unitOfWork;
+    private readonly IMapper mapper;
+    private readonly JwtHandler jwtHandler;
+
+    public UserService(IUserRepository userRepository, IUnitOfWork unitOfWork, IMapper mapper, JwtHandler jwtHandler)
+    {
+        this.userRepository = userRepository;
+        this.unitOfWork = unitOfWork;
+        this.mapper = mapper;
+        this.jwtHandler = jwtHandler;
+    }
+    
+    public async Task<IEnumerable<User>> ListAsync()
+    {
+        return await userRepository.ListAsync();
+    }
+
+    public async Task<LoginResponse> LoginAsync(LoginRequest model)
+    {
+        // obtiene el usuario por su email
+        var user = await userRepository.FindByEmailAsync(model.Email);
+        
+        // valida que el usuario exista y que la contraseña sea correcta
+        if (user == null || !BCryptNet.Verify(model.Password, user.Password))
+        {
+            throw new AppException(HttpStatusCode.BadRequest, "Email or password is incorrect");
+        }
+ 
+        // si el usuario existe y la contraseña es correcta, genera el token
+        var response = mapper.Map<LoginResponse>(user);
+        response.Token = jwtHandler.GenerateToken(user);
+        
+        return response;
+    }
+
+    public async Task<User> GetByIdAsync(int id)
+    {
+        var user = await userRepository.FindByIdAsync(id);
+        if (user == null) throw new KeyNotFoundException("User not found");
+        return user;
+    }
+
+    public async Task<UserApiResponse> RegisterAsync(RegisterRequest model)
+    {
+        // valida que el email no esté registrado
+        if (userRepository.ExistsByEmail(model.Email))
+        {
+            throw new AppException(HttpStatusCode.Conflict, $"Email '{model.Email}' is already taken");
+        } 
+        
+        // mapea el modelo a un objeto User
+        var user = mapper.Map<User>(model);
+        
+        // hashear la contraseña
+        user.Password = BCryptNet.HashPassword(model.Password);
+        
+        // registra el usuario
+        try
+        {
+            await userRepository.AddAsync(user);
+            await unitOfWork.CompleteAsync();
+            return new UserApiResponse(user);
+        }
+        catch (Exception e)
+        {
+            throw new AppException(HttpStatusCode.InternalServerError, $"An error occurred while saving the user: {e.Message}");
+        }
+    }
+
+    public async Task<UserApiResponse> UpdateAsync(int id, UpdateUserRequest model)
+    {
+        // obtiene el usuario por su id
+        var user = GetById(id);
+        
+        // valida que el email no esté registrado
+        if (userRepository.ExistsByEmail(model.Email))
+        {
+            throw new AppException(HttpStatusCode.Conflict, $"Email '{model.Email}' is already taken");
+        }
+        
+        // mapea el modelo a un objeto User
+        mapper.Map(model, user);
+        
+        // actualiza el usuario
+        try
+        {
+            userRepository.Update(user);
+            await unitOfWork.CompleteAsync();
+            return new UserApiResponse(user);
+        }
+        catch (Exception e)
+        {
+            throw new AppException(HttpStatusCode.InternalServerError, $"An error occurred while updating the user: {e.Message}");
+        }
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        // obtiene el usuario por su id
+        var user = GetById(id);
+ 
+        // elimina el usuario
+        try
+        {
+            userRepository.Remove(user);
+            await unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new AppException(HttpStatusCode.InternalServerError, $"An error occurred while deleting the user: {e.Message}");
+        }
+    }
+    
+    // helper methods
+    private User GetById(int id)
+    {
+        var user = userRepository.FindById(id);
+        if (user == null) throw new AppException(HttpStatusCode.NotFound, "User not found");
+        return user;
+    }
+}
